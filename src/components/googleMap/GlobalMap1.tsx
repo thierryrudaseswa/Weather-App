531268aadbe690b3621ccd1811ff06ac2f60ebaf
aurumdev952 <117060315+Aurumdev952@users.noreply.github.com>
2023-07-22 21:40:01 +0200

    changes

--- src/components/googleMap/GlobalMap1.tsx
+++ src/components/googleMap/GlobalMap1.tsx
@@ -1,21 +1,28 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "../../App.css";
 import "leaflet/dist/leaflet.css";
-import { MapContainer, TileLayer, Marker, Popup, useMapEvents } from "react-leaflet";
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+  useMapEvents,
+} from "react-leaflet";
 import MarkerClusterGroup from "react-leaflet-cluster";
 import { divIcon, DivIcon, point, Icon } from "leaflet";
 import { geocode } from "opencage-api-client";
+import axios from "axios";
 
 const customIcon = new Icon({
-  iconUrl: "https://as2.ftcdn.net/v2/jpg/05/72/20/81/1000_F_572208185_2NVmGnwrBykudKF8ytKyIxfoQak60DsN.jpg",
-  iconSize: [38, 38]
+  iconUrl:
+    "https://as2.ftcdn.net/v2/jpg/05/72/20/81/1000_F_572208185_2NVmGnwrBykudKF8ytKyIxfoQak60DsN.jpg",
+  iconSize: [38, 38],
 });
-
 const createClusterCustomIcon = function (cluster: any): DivIcon {
   return divIcon({
     html: `<span class="cluster-icon">${cluster.getChildCount()}</span>`,
     className: "custom-marker-cluster",
-    iconSize: point(33, 33, true)
+    iconSize: point(33, 33, true),
   });
 };
 
@@ -28,7 +35,7 @@ const citiesByContinent: { continent: string; cities: MarkerData[] }[] = [
     continent: "Europe",
     cities: [
       { geocode: [48.86, 2.3522], popUp: "Paris - Hello, I am pop up 1" },
-      { geocode: [52.5200, 13.4050], popUp: "Berlin - Hello, I am pop up 2" },
+      { geocode: [52.52, 13.405], popUp: "Berlin - Hello, I am pop up 2" },
       { geocode: [41.9028, 12.4964], popUp: "Rome - Hello, I am pop up 3" },
       { geocode: [51.5074, -0.1278], popUp: "London - Hello, I am pop up 4" },
       { geocode: [48.2082, 16.3738], popUp: "Vienna - Hello, I am pop up 5" },
@@ -39,10 +46,16 @@ const citiesByContinent: { continent: string; cities: MarkerData[] }[] = [
     continent: "Africa",
     cities: [
       { geocode: [6.5244, 3.3792], popUp: "Lagos - Hello, I am pop up 6" },
-      { geocode: [-33.9249, 18.4241], popUp: "Cape Town - Hello, I am pop up 7" },
+      {
+        geocode: [-33.9249, 18.4241],
+        popUp: "Cape Town - Hello, I am pop up 7",
+      },
       { geocode: [25.2048, 55.2708], popUp: "Dubai - Hello, I am pop up 8" },
       { geocode: [-1.2921, 36.8219], popUp: "Nairobi - Hello, I am pop up 9" },
-      { geocode: [-33.918861, 18.423300], popUp: "Casablanca - Hello, I am pop up 10" },
+      {
+        geocode: [-33.918861, 18.4233],
+        popUp: "Casablanca - Hello, I am pop up 10",
+      },
       // Add more cities...
     ],
   },
@@ -50,34 +63,71 @@ const citiesByContinent: { continent: string; cities: MarkerData[] }[] = [
     continent: "Asia",
     cities: [
       { geocode: [35.6895, 139.6917], popUp: "Tokyo - Hello, I am pop up 11" },
-      { geocode: [37.7749, -122.4194], popUp: "San Francisco - Hello, I am pop up 12" },
-      { geocode: [19.0760, 72.8777], popUp: "Mumbai - Hello, I am pop up 13" },
-      { geocode: [13.7563, 100.5018], popUp: "Bangkok - Hello, I am pop up 14" },
-      { geocode: [31.2304, 121.4737], popUp: "Shanghai - Hello, I am pop up 15" },
+      {
+        geocode: [37.7749, -122.4194],
+        popUp: "San Francisco - Hello, I am pop up 12",
+      },
+      { geocode: [19.076, 72.8777], popUp: "Mumbai - Hello, I am pop up 13" },
+      {
+        geocode: [13.7563, 100.5018],
+        popUp: "Bangkok - Hello, I am pop up 14",
+      },
+      {
+        geocode: [31.2304, 121.4737],
+        popUp: "Shanghai - Hello, I am pop up 15",
+      },
       // Add more cities...
     ],
   },
   {
     continent: "America",
     cities: [
-      { geocode: [40.7128, -74.0060], popUp: "New York City - Hello, I am pop up 16" },
-      { geocode: [34.0522, -118.2437], popUp: "Los Angeles - Hello, I am pop up 17" },
-      { geocode: [19.4326, -99.1332], popUp: "Mexico City - Hello, I am pop up 18" },
-      { geocode: [43.6532, -79.3832], popUp: "Toronto - Hello, I am pop up 19" },
-      { geocode: [-34.6037, -58.3816], popUp: "Buenos Aires - Hello, I am pop up 20" },
+      {
+        geocode: [40.7128, -74.006],
+        popUp: "New York City - Hello, I am pop up 16",
+      },
+      {
+        geocode: [34.0522, -118.2437],
+        popUp: "Los Angeles - Hello, I am pop up 17",
+      },
+      {
+        geocode: [19.4326, -99.1332],
+        popUp: "Mexico City - Hello, I am pop up 18",
+      },
+      {
+        geocode: [43.6532, -79.3832],
+        popUp: "Toronto - Hello, I am pop up 19",
+      },
+      {
+        geocode: [-34.6037, -58.3816],
+        popUp: "Buenos Aires - Hello, I am pop up 20",
+      },
       // Add more cities...
     ],
   },
   // Add more continents and cities...
 ];
 
-
-async function reverseGeocode(lat: number, lng: number): Promise<string> {
+async function reverseGeocode(
+  lat: number,
+  lng: number
+): Promise<{ city: string; lat: number; lng: number; continent: string }> {
   try {
-    const response = await geocode({ q: `${lat},${lng}`, key: "9b30c2b34a7e42d5a47e37cf98d7ad58" });
+    const response = await geocode({
+      q: `${lat},${lng}`,
+      key: "9b30c2b34a7e42d5a47e37cf98d7ad58",
+    });
+    console.log("fetching", response.results);
+
     if (response.status.code === 200 && response.results.length > 0) {
-      const city = response.results[0].components.city;
-      return city;
+      const city =
+        response.results[0].components.city ??
+        response.results[0].components.state;
+      return {
+        city,
+        ...response.results[0].geometry,
+        continent: response.results[0].components.continent,
+      };
     } else {
       throw new Error("Reverse geocoding failed");
     }
@@ -88,10 +138,42 @@ async function reverseGeocode(lat: number, lng: number): Promise<string> {
 }
 
 export default function GlobalMap() {
+  const [data, setData] = useState(citiesByContinent);
+  console.log(process.env.REACT_APP_API_KEY_OPENWEATHER);
+  
   const handleMapClick = async (event: any) => {
     const { lat, lng } = event.latlng;
     try {
-      const city = await reverseGeocode(lat, lng);
+      const {
+        city,
+        continent,
+        lat: lat2,
+        lng: lng2,
+      } = await reverseGeocode(lat, lng);
+
+      const res = await axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat2}&lon=${lng2}&appid=${process.env.REACT_APP_API_KEY_OPENWEATHER}&units=metric`
+      );
+      const data = await res.data;
+      setData((prev) => {
+        const newData = prev.map((cont) => {
+          let weatherData: null | { temp: number; main: string } = null;
+          if (res.status === 200) {
+            weatherData = { main: data.weather[0].main, temp: data.main.temp };
+            console.log(weatherData);
+          }
+          if (cont.continent === continent) {
+            cont.cities.push({
+              geocode: [lat, lng],
+              popUp: `City: ${city}, temp: ${weatherData?.temp} degress, weather: ${weatherData?.main}`,
+            });
+            return cont;
+          } else {
+            return cont;
+          }
+        });
+        return newData;
+      });
       console.log(`Latitude: ${lat}, Longitude: ${lng}, City: ${city}`);
     } catch (error) {
       console.error(error);
@@ -100,7 +182,7 @@ export default function GlobalMap() {
 
   function MapClickHandler() {
     const map = useMapEvents({
-      click: handleMapClick
+      click: handleMapClick,
     });
 
     useEffect(() => {
@@ -117,14 +199,21 @@ export default function GlobalMap() {
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
       <MapClickHandler />
-      <MarkerClusterGroup chunkedLoading iconCreateFunction={createClusterCustomIcon}>
-        {citiesByContinent.map((continentData, continentIndex) => (
+      <MarkerClusterGroup
+        chunkedLoading
+        iconCreateFunction={createClusterCustomIcon}
+      >
+        {data.map((continentData, continentIndex) =>
           continentData.cities.map((city, cityIndex) => (
-            <Marker key={`${continentIndex}-${cityIndex}`} position={city.geocode} icon={customIcon}>
+            <Marker
+              key={`${continentIndex}-${cityIndex}`}
+              position={city.geocode}
+              icon={customIcon}
+            >
               <Popup>{city.popUp}</Popup>
             </Marker>
           ))
-        ))}
+        )}
       </MarkerClusterGroup>
     </MapContainer>
   );
